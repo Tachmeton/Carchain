63a060a272df00ab48c9363a06e57e893f4e95c4
Tachmeton <40113390+Tachmeton@users.noreply.github.com>
2020-03-08 15:37:44 +0100

    Introduction of modifier to shorten require Statements and get a little bit of Rights on the Smart Contract.

--- Blockchain/ethereum/contracts/carchain.sol
+++ Blockchain/ethereum/contracts/carchain.sol
@@ -20,7 +20,7 @@ enum CarState {
     int256 latitude;
   }
 
-  mapping (uint256 => Car) carpool;
+  mapping (address => Car) carpool;
 
 
   constructor() public {
@@ -32,9 +32,9 @@ enum CarState {
   /*
   Adds a new Car to the Carpool.
   */
-  function addCar(uint256 identifierCar) public {
+  function addCar(address identifierCar) public {
     require(
-      carpool[identifierCar].owner == address(0), "Car is already in Carpool and a new Car can not be added."
+      carpool[identifierCar].owner == address(0), "ID already used."
       );
 
     Car memory newCar = Car({
@@ -47,7 +47,7 @@ enum CarState {
   /*
   Removes a car which is not currently in Use from the total Carpool.
   */
-  function removeCar(uint256 identifierCar, address identifierOwner) public{
+  function removeCar(address identifierCar, address identifierOwner) public knownCar(identifierCar) onlyOwner (identifierCar, identifierOwner) {
     require(
       carpool[identifierCar].currentState == CarState.Free && carpool[identifierCar].leaser == address(0) && carpool[identifierCar].timeRented == 0,"Car still in use and can not be removed."
       );
@@ -67,24 +67,21 @@ enum CarState {
   /*
   Marks a car as rented and safes the point from where the car was rented.
   */
-  function rentCar(uint256 identifierCar) public payable {
-    require(carpool[identifierCar].owner != address(0), "Car is not in carpool");
+  function rentCar(address identifierCar) public knownCar(identifierCar) payable {
     require(getCurrentState(identifierCar) == CarState.Free, "Car has no one who leased it.");
     //Mindestmietdauer?
     require(msg.value > 1800, "You did not pay enough. (1800)");
 
     carpool[identifierCar].currentState = CarState.Leased;
     carpool[identifierCar].leaser = msg.sender;
-    //Problem: block.timestamp ist ungefähr 90 sekunden ungenau wegen manipulation von minern.
+    //Problem: block.timestamp ist worst case 90 sekunden ungenau wegen manipulation von minern.
     carpool[identifierCar].timeRented = getTimeNow() + msg.value;
   }
 
   /*
   Gives a boolean value if a car is now avaible for rent.
   */
-  function mayRent(uint256 identifierCar) public view returns (bool) {
-    require(carpool[identifierCar].owner != address(0), "Car is not in carpool");
-
+  function mayRent(address identifierCar) public knownCar(identifierCar) view returns (bool) {
     if(carpool[identifierCar].leaser == address(0) && getCurrentState(identifierCar) == CarState.Free){
       return true;
     }else{
@@ -95,9 +92,7 @@ enum CarState {
   /*
   Gives a boolean value if a Leaser is still Lawfully/ Legally a Leaser and has enough credit.
   */
-  function isLegalLeaser(uint256 identifierCar, address identifierLeaser) public view returns (bool) {
-    require(carpool[identifierCar].owner > address(0), "Car is not in carpool");
-    require(msg.sender == identifierLeaser, "Sender is not the same as the Parameter Leaser");
+  function isLegalLeaser(address identifierCar, address identifierLeaser) public knownCar(identifierCar) view returns (bool) {
     require(getCurrentState(identifierCar) == CarState.Leased, "Car is not in use. --> There can not be a leaser.");
 
     if(carpool[identifierCar].leaser == identifierLeaser && carpool[identifierCar].timeRented < getTimeNow()){
@@ -109,8 +104,8 @@ enum CarState {
   /*
   After a Car was rented the car is given back to the free carpool and given the state not rented.
   */
-  function returnCarToCarpool(uint256 identifierCar) public payable {
-    require(carpool[identifierCar].leaser > address(0) && getCurrentState(identifierCar) == CarState.Leased, "Car is not in Use and therefore can not be returned.");
+  function returnCarToCarpool(address identifierCar, address identifierLeaser) public
+            knownCar(identifierCar) isLeased(identifierCar) isLeasedBy(identifierCar, identifierLeaser) payable {
 
     carpool[identifierCar].timeRented = 0;
     carpool[identifierCar].owner.transfer(carpool[identifierCar].amountPaid);
@@ -125,30 +120,30 @@ enum CarState {
 
 //getter and setter for Car
 
-  function getOwner(uint256 identifierCar) public view returns (address) {
+  function getOwner(address identifierCar) public knownCar(identifierCar) view returns (address) {
     return carpool[identifierCar].owner;
   }
 
   /*
   Checks if a Car is already rented/ in Use.
   */
-  function getCurrentState(uint256 identifierCar) public view returns (CarState) {
+  function getCurrentState(address identifierCar) public knownCar(identifierCar) view returns (CarState) {
     return carpool[identifierCar].currentState;
   }
 
-  function getLeaser(uint256 identifierCar) public view returns (address) {
+  function getLeaser(address identifierCar) public knownCar(identifierCar) view returns (address) {
     return carpool[identifierCar].leaser;
   }
 
-  function getTimeRented(uint256 identifierCar) public view returns (uint256) {
+  function getTimeRented(address identifierCar) public knownCar(identifierCar) view returns (uint256) {
     return carpool[identifierCar].timeRented;
   }
 
-  function getLatitude(uint256 identifierCar) public view returns (int256) {
+  function getLatitude(address identifierCar) public knownCar(identifierCar) view returns (int256) {
     return carpool[identifierCar].latitude;
   }
 
-  function getLongitude(uint256 identifierCar) public view returns (int256) {
+  function getLongitude(address identifierCar) public knownCar(identifierCar) view returns (int256) {
     return carpool[identifierCar].longitude;
   }
 
@@ -159,7 +154,7 @@ enum CarState {
   Resets a car from the total Carpool.
   Just for debugging Reasons. Should not make to the final version.
   */
-  function resetCar(uint256 identifierCar) public{
+  function resetCar(address identifierCar) public{
     Car memory defaultValueCar = Car({
       owner: address(0), currentState: CarState.Free, leaser: address(0), timeRented: 0, amountPaid: 0, longitude: 0, latitude: 0
       });
@@ -167,8 +162,30 @@ enum CarState {
     carpool[identifierCar] = defaultValueCar;
   }
 
-  function getTimeNow() private returns (uint256){
+  function getTimeNow() private view returns (uint256){
     return now;
   }
+
+
+  modifier knownCar (address identifierCar) {
+    require(carpool[identifierCar].owner != address(0), "Car is not in carpool");
+    _;
+  }
+
+  modifier onlyOwner (address identifierCar, address identifierOwner) {
+    require(carpool[identifierCar].owner == identifierOwner, "Not the right owner of the Car.");
+    _;
+  }
+
+  modifier isLeased (address identifierCar) {
+    require(getCurrentState(identifierCar) == CarState.Leased, "Car is not Leased");
+    _;
+  }
+
+  modifier isLeasedBy (address identifierCar, address identifierLeaser) {
+    require(getLeaser(identifierCar) == identifierLeaser, "Not the right Leaser of the Car.");
+    _;
+  }
+
 }
 
