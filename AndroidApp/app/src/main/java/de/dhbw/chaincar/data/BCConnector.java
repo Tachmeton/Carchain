18d0e54549135047862dc5dde42967f964abca9e
Simon Gaugler <simon@smagcloud.de>
2020-04-08 15:00:09 +0200

    misc.xml, build.gradle und 2 weitere dateien aktualisiert...

--- AndroidApp/app/src/main/java/de/dhbw/chaincar/data/BCConnector.java
+++ AndroidApp/app/src/main/java/de/dhbw/chaincar/data/BCConnector.java
@@ -3,6 +3,8 @@ package de.dhbw.chaincar.data;
 
 import android.content.Context;
 
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.ECKeyPair;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
 
@@ -12,46 +14,41 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.math.BigInteger;
 
 import de.dhbw.chaincar.R;
 
 public class BCConnector {
 
-    String contractAdress =     "0xdb268b971C46d61bd512071D57706dEADe11369B";
-    String fromAddress =        "0x28f814Ff05aF5DbFF0401A6c98AB0942DbA95a63";
-    String renterAddress =      "0x3d48704143135059A1990dcDF9eEC5C73f750179";
-    String carWallet =          "0xfCB0306AadaFF0CD11A9576180bbd6a7787ca509";
+    private String contractAdress = "0xdb268b971C46d61bd512071D57706dEADe11369B";
+    private String fromAddress = "0x28f814Ff05aF5DbFF0401A6c98AB0942DbA95a63";
+    private String renterAddress = "0x3d48704143135059A1990dcDF9eEC5C73f750179";
+    private String renterPrivKey = "0xef56dc06b9c5c9b38ac952627988397d9b735b902c7263e3600ef808e41193f9";
+    private String carWallet = "0xfCB0306AadaFF0CD11A9576180bbd6a7787ca509";
+    private BigInteger gas_price = new BigInteger("6000000");
+    private BigInteger gas_limit = new BigInteger("6000000000");
+
 
     private Context context;
     private static BCConnector bccon;
+    private Carchain contract;
 
     public static BCConnector getInstance(Context context) {
-        if(bccon == null){
+        if (bccon == null) {
             bccon = new BCConnector(context);
         }
         return bccon;
     }
-    private BCConnector(Context context){
+
+    private BCConnector(Context context) {
         this.context = context;
         Web3j web3 = Web3j.build(new HttpService("http://193.196.54.51:8545"));
-        
+        this.contract = Carchain.load(contractAdress, web3, Credentials.create(renterPrivKey, renterAddress), gas_price, gas_limit);
     }
 
-    public int getConnector(){
-
-
-
-
-        return 0;
+    public Carchain getContract() {
+        return contract;
     }
 
 
-    private String readContractJSON(){
-        InputStream is = context.getResources().openRawResource(R.raw.Carchain);
-    }   Writer writer = new StringWriter();
-        char[] buffer = new char[1024];
-        try {
-            Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-            int n;
-}           while ((n = reader.read(buffer)) != -1) {
-                writer.write(buffer, 0, n);
+}
--- AndroidApp/app/src/main/java/de/dhbw/chaincar/data/VehicleLoader.java
+++ AndroidApp/app/src/main/java/de/dhbw/chaincar/data/VehicleLoader.java
@@ -2,30 +2,55 @@ package de.dhbw.chaincar.data;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.util.Log;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
 
 public class VehicleLoader {
 
     private static VehicleLoader instance;
+    private Context context;
+    private static String TAG = "data.VehicleLoader";
 
     public static VehicleLoader getInstance(Context context) {
-        if(instance == null){
-            instance = new VehicleLoader();
+        if (instance == null) {
+            instance = new VehicleLoader(context);
         }
         return instance;
     }
 
-    private VehicleLoader(){
-
+    private VehicleLoader(Context context) {
+        this.context = context;
     }
 
-    public List<Vehicle> getVehicles(){
+    public List<Vehicle> getVehicles() throws Exception {
+        List<Vehicle> vehicles = new ArrayList<>();
+        Carchain contract = BCConnector.getInstance(context).getContract();
+        contract.getAvailableVehicles().send().forEach(bigIntId -> {
+            Log.d(TAG, "Loading Data for Car " + bigIntId);
+            try {
+                String id = bigIntId.toString();
+                String name = contract.getNummernschild(id).send();
+                String homeCountry = "DE";
+                String homeCity = "";
+                String homePostCode = "";
+                String homeStreet = "";
+                String homeStreetNumber = "";
+                float pricePerHour = contract.getMietpreis(id).send().floatValue();
+                float minRentDuration = contract.getMinMietdauer(id).send().floatValue();
+                float maxRentDuration = contract.getMaxMietdauer(id).send().floatValue();
 
-    }
-
-    private Drawable[] getPicturesForVehicle(){
-
+                Vehicle v = new Vehicle(id, name, homeCountry, homeCity, homePostCode,
+                        homeStreet, homeStreetNumber, pricePerHour, minRentDuration, maxRentDuration);
+                Log.d(TAG, "New Vehicle created, adding it now! Vehicle: " + v.toString());
+                vehicles.add(v);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        return vehicles;
     }
 
 
